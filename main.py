bf089f50d8db91577bbcd4a65cc2a079a8b93863
Ahmedm4x <90485375+Ahmedm4x@users.noreply.github.com>
2022-03-28 00:31:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,11 @@ def START(msg,chat_id):
         bot.sendMessage(chat_id,'send name and name of artist like this: \nName song\nor for better search use this:\nName song - Name artist')
     elif msg == '/artist':
         sort[chat_id]='artist'
-        bot.sendMessage(chat_id,'send name and name of artist like this: \nName artist join to out channel @spotifyMusicNews')
+        bot.sendMessage(chat_id,'send name and name of artist like this: \nName artist')
+
+    elif msg == "/channel":
+        sort[chat_id]='channel'
+        bot.sendMessage(chat_id, 'Join to our channel @spotifymusicnews')
 
     else:
         try:
